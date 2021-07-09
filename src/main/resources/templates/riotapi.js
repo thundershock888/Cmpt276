b4a2b3628760c4312d0a50c8205cefb02d446322
Sunderinq <tegnoor.gill@hotmail.com>
2021-07-09 04:35:51 -0700

    Added multiple api's
    gets summoner data, match data, list of all games played, and ranked data.

--- src/main/resources/templates/riotapi.js
+++ src/main/resources/templates/riotapi.js
@@ -1,59 +1,63 @@
-const key = "api_key=RGAPI-f73e856b-f7c1-4661-87bb-40f4e089e45e";
-const sp = "%20";
-const fetch = require("node-fetch");
-const sumName= document.querySelector('input').value;
-
-getMatchList(fetchSumByName(sumName, 'accountId'));
-
-async function getMatchList(accId, champID, queue, endTime, beginTime, endIndex, beginIndex){
-    let link = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + await accid + "?";
-    if(champID != null)
-        link+= 'champion=' +champID+'&';
-    if(queue != null)
-        link+= 'queue=' +queue+'&';
-    if(endTime != null)
-        link+= 'endTime=' +endTime+'&';
-    if(beginTime != null)
-        link+= 'beginTime=' +beginTime+'&';
-    if(endIndex != null)
-        link+= 'endIndex=' +endIndex+'&';
-    if(beginIndex != null)
-        link+= 'beginIndex=' +beginIndex+'&';
-    link += key;
-
+function requestSummonerData(summonerName, key){//helps get summoner info 
+    let link = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName+"?api_key="+ key;
     let response = await fetch(link);
     response = await response.json();
-    console.log(response.matches[0].champion);
+    return response;
+}
 
+function requestMatchData(matchID, key){//helps get specific match data
+    let link = "https://na1.api.riotgames.com/lol/match/v4/matches/"+ matchID+"?api_key="+key;
+    let response = await fetch(link);
+    response = await response.json();
+    return response;
+}
+
+function requestMatchListData(accountID, key){//gets list of matches
+    let link = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/"+ summonerID+"?api_key="+key;
+    let response = await fetch(link);
+    response = await response.json();
+    return response;
+}
+
+function requestRankedLeagueData(summonerID, key){//gets data of player rank in competitive
+    let link = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ summonerID+"?api_key="+key;
+    let response = await fetch(link);
+    response = await response.json();
+    return response;
 }
 
 
-async function fetchSumByName(name, ch){
+function summonerInfo(){//function is called when summoner name is entered and thus 
+    const key = "RGAPI-3934ee96-4600-4722-9053-b5626cb4a6fd";//api key
+    const sp = "%20";
+    const fetch = require("node-fetch");
+    const name= document.getElementById('summonerSearch').value;
+
     while(name.includes(" ")){
         let spaceSpot = name.indexOf(" ");
         name - name.substring(0,spaceSpot) + sp + name.substring(spaceSpot+1);
     }
 
-    const link = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?${key}'
-    const resposne = await fetch(link);
+    const responseJSON = requestSummonerData(name, key);
+    //different ids corressponding to function values
+    const accountID = responseJSON["accountId"];
+    const summonerLevel = responseJSON["summonerLevel"];
+    const summonerID = responseJSON["id"];
 
-    let data = await resposne.json();
+    const responseJSON2 = requestRankedLeagueData(summonerID, key);
+    const rankTier = responseJSON2["tier"];
+    const rankDivision = responseJSON2["rank"];
+    const rankPoint = responseJSON2["leaguePoints"];
+    const rankWins = responseJSON["wins"];
+    const rankLosses = responseJSON2["losses"];
 
-    if(ch == "id"){
-        console.log("wrong one reached")
-        return data.id;
-    } else if(ch == 'puuuid'){
-        return data.puuuid;
-    }else if(ch = 'name'){
-        return data.name;
-    }else if(ch == 'profileIconId'){
-        return data.profileIconId;
-    }else if(ch == 'revisionDate'){
-        return data.revisionDate;
-    }else if(ch == 'summonerLevel'){
-        return data.summonerLevel;
-    }else{
-        console.log("ch is not valid");
-        return null;
-    }
+    console.log("{ accountId: "+ accountID + " },");
+    console.log("{ summonerLevel: "+ summonerLevel + " },");
+    console.log("{ summonerID: "+ summonerID + " },");
+    console.log("{ rankTier: "+ rankTier + " },");
+    console.log("{ rankDivision: "+ rankDivision + " },");
+    console.log("{ rankPoint: "+ rankPoint + " },");
+    console.log("{ rankWins: "+ rankWins + " },");
+    console.log("{ rankLosses: "+ rankLosses + " },");
+
 }
