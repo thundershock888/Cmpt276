4d2d59a80f4fe578e39cdb3b9454388d11767252
Wayne Li <wayne121542@gmail.com>
2021-07-23 13:39:01 -0700

    added classes for champions

---
+++ src/main/java/champs/Aatrox.java
@@ -0,0 +1,61 @@
+package champs;
+
+import java.util.List;
+
+public class Aatrox{
+	private Image image;
+	private Stats stats;
+	private String partype;
+	private String name;
+	private String id;
+	private String title;
+	private String blurb;
+	private String version;
+	private String key;
+	private Info info;
+	private List<String> tags;
+
+	public Image getImage(){
+		return image;
+	}
+
+	public Stats getStats(){
+		return stats;
+	}
+
+	public String getPartype(){
+		return partype;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	public String getBlurb(){
+		return blurb;
+	}
+
+	public String getVersion(){
+		return version;
+	}
+
+	public String getKey(){
+		return key;
+	}
+
+	public Info getInfo(){
+		return info;
+	}
+
+	public List<String> getTags(){
+		return tags;
+	}
+}
---
+++ src/main/java/champs/Data.java
@@ -0,0 +1,784 @@
+package champs;
+
+public class Data{
+	private Jax jax;
+	private Sona sona;
+	private Tristana tristana;
+	private Yuumi yuumi;
+	private Seraphine seraphine;
+	private Varus varus;
+	private Kaisa kaisa;
+	private Fiora fiora;
+	private Singed singed;
+	private Samira samira;
+	private TahmKench tahmKench;
+	private Leblanc leblanc;
+	private Thresh thresh;
+	private Karma karma;
+	private Lillia lillia;
+	private Jhin jhin;
+	private Rumble rumble;
+	private Udyr udyr;
+	private LeeSin leeSin;
+	private Yorick yorick;
+	private Ornn ornn;
+	private Kayn kayn;
+	private Neeko neeko;
+	private Kassadin kassadin;
+	private Sivir sivir;
+	private MissFortune missFortune;
+	private Senna senna;
+	private Draven draven;
+	private Yasuo yasuo;
+	private Kayle kayle;
+	private Rell rell;
+	private Shaco shaco;
+	private Renekton renekton;
+	private Hecarim hecarim;
+	private Fizz fizz;
+	private KogMaw kogMaw;
+	private Yone yone;
+	private Maokai maokai;
+	private Lissandra lissandra;
+	private Jinx jinx;
+	private Urgot urgot;
+	private Fiddlesticks fiddlesticks;
+	private Galio galio;
+	private Pantheon pantheon;
+	private Talon talon;
+	private Gangplank gangplank;
+	private Sett sett;
+	private Ezreal ezreal;
+	private Gnar gnar;
+	private Teemo teemo;
+	private Annie annie;
+	private Mordekaiser mordekaiser;
+	private Azir azir;
+	private Kennen kennen;
+	private Riven riven;
+	private Chogath chogath;
+	private Aatrox aatrox;
+	private Poppy poppy;
+	private Taliyah taliyah;
+	private Illaoi illaoi;
+	private Pyke pyke;
+	private Heimerdinger heimerdinger;
+	private Alistar alistar;
+	private XinZhao xinZhao;
+	private Lucian lucian;
+	private Volibear volibear;
+	private Sejuani sejuani;
+	private Nidalee nidalee;
+	private Garen garen;
+	private Leona leona;
+	private Zed zed;
+	private Blitzcrank blitzcrank;
+	private Rammus rammus;
+	private Velkoz velkoz;
+	private Caitlyn caitlyn;
+	private Trundle trundle;
+	private Kindred kindred;
+	private Akshan akshan;
+	private Quinn quinn;
+	private Ekko ekko;
+	private Nami nami;
+	private Swain swain;
+	private Aphelios aphelios;
+	private Sylas sylas;
+	private Taric taric;
+	private Syndra syndra;
+	private Rakan rakan;
+	private Skarner skarner;
+	private Gwen gwen;
+	private Braum braum;
+	private Veigar veigar;
+	private Xerath xerath;
+	private Corki corki;
+	private Nautilus nautilus;
+	private Ahri ahri;
+	private Jayce jayce;
+	private Darius darius;
+	private Tryndamere tryndamere;
+	private Janna janna;
+	private Elise elise;
+	private Vayne vayne;
+	private Brand brand;
+	private Zoe zoe;
+	private Graves graves;
+	private Soraka soraka;
+	private Xayah xayah;
+	private Viego viego;
+	private Karthus karthus;
+	private Vladimir vladimir;
+	private Zilean zilean;
+	private Katarina katarina;
+	private Shyvana shyvana;
+	private Warwick warwick;
+	private Ziggs ziggs;
+	private Kled kled;
+	private Khazix khazix;
+	private Olaf olaf;
+	private TwistedFate twistedFate;
+	private Nunu nunu;
+	private Qiyana qiyana;
+	private Rengar rengar;
+	private Bard bard;
+	private Irelia irelia;
+	private Ivern ivern;
+	private MonkeyKing monkeyKing;
+	private Ashe ashe;
+	private Kalista kalista;
+	private Akali akali;
+	private Vi vi;
+	private Amumu amumu;
+	private Lulu lulu;
+	private Morgana morgana;
+	private Nocturne nocturne;
+	private Diana diana;
+	private AurelionSol aurelionSol;
+	private Zyra zyra;
+	private Viktor viktor;
+	private Cassiopeia cassiopeia;
+	private Nasus nasus;
+	private Twitch twitch;
+	private DrMundo drMundo;
+	private Orianna orianna;
+	private Evelynn evelynn;
+	private RekSai rekSai;
+	private Lux lux;
+	private Sion sion;
+	private Camille camille;
+	private MasterYi masterYi;
+	private Ryze ryze;
+	private Malphite malphite;
+	private Anivia anivia;
+	private Shen shen;
+	private JarvanIV jarvanIV;
+	private Malzahar malzahar;
+	private Zac zac;
+	private Gragas gragas;
+
+	public Jax getJax(){
+		return jax;
+	}
+
+	public Sona getSona(){
+		return sona;
+	}
+
+	public Tristana getTristana(){
+		return tristana;
+	}
+
+	public Yuumi getYuumi(){
+		return yuumi;
+	}
+
+	public Seraphine getSeraphine(){
+		return seraphine;
+	}
+
+	public Varus getVarus(){
+		return varus;
+	}
+
+	public Kaisa getKaisa(){
+		return kaisa;
+	}
+
+	public Fiora getFiora(){
+		return fiora;
+	}
+
+	public Singed getSinged(){
+		return singed;
+	}
+
+	public Samira getSamira(){
+		return samira;
+	}
+
+	public TahmKench getTahmKench(){
+		return tahmKench;
+	}
+
+	public Leblanc getLeblanc(){
+		return leblanc;
+	}
+
+	public Thresh getThresh(){
+		return thresh;
+	}
+
+	public Karma getKarma(){
+		return karma;
+	}
+
+	public Lillia getLillia(){
+		return lillia;
+	}
+
+	public Jhin getJhin(){
+		return jhin;
+	}
+
+	public Rumble getRumble(){
+		return rumble;
+	}
+
+	public Udyr getUdyr(){
+		return udyr;
+	}
+
+	public LeeSin getLeeSin(){
+		return leeSin;
+	}
+
+	public Yorick getYorick(){
+		return yorick;
+	}
+
+	public Ornn getOrnn(){
+		return ornn;
+	}
+
+	public Kayn getKayn(){
+		return kayn;
+	}
+
+	public Neeko getNeeko(){
+		return neeko;
+	}
+
+	public Kassadin getKassadin(){
+		return kassadin;
+	}
+
+	public Sivir getSivir(){
+		return sivir;
+	}
+
+	public MissFortune getMissFortune(){
+		return missFortune;
+	}
+
+	public Senna getSenna(){
+		return senna;
+	}
+
+	public Draven getDraven(){
+		return draven;
+	}
+
+	public Yasuo getYasuo(){
+		return yasuo;
+	}
+
+	public Kayle getKayle(){
+		return kayle;
+	}
+
+	public Rell getRell(){
+		return rell;
+	}
+
+	public Shaco getShaco(){
+		return shaco;
+	}
+
+	public Renekton getRenekton(){
+		return renekton;
+	}
+
+	public Hecarim getHecarim(){
+		return hecarim;
+	}
+
+	public Fizz getFizz(){
+		return fizz;
+	}
+
+	public KogMaw getKogMaw(){
+		return kogMaw;
+	}
+
+	public Yone getYone(){
+		return yone;
+	}
+
+	public Maokai getMaokai(){
+		return maokai;
+	}
+
+	public Lissandra getLissandra(){
+		return lissandra;
+	}
+
+	public Jinx getJinx(){
+		return jinx;
+	}
+
+	public Urgot getUrgot(){
+		return urgot;
+	}
+
+	public Fiddlesticks getFiddlesticks(){
+		return fiddlesticks;
+	}
+
+	public Galio getGalio(){
+		return galio;
+	}
+
+	public Pantheon getPantheon(){
+		return pantheon;
+	}
+
+	public Talon getTalon(){
+		return talon;
+	}
+
+	public Gangplank getGangplank(){
+		return gangplank;
+	}
+
+	public Sett getSett(){
+		return sett;
+	}
+
+	public Ezreal getEzreal(){
+		return ezreal;
+	}
+
+	public Gnar getGnar(){
+		return gnar;
+	}
+
+	public Teemo getTeemo(){
+		return teemo;
+	}
+
+	public Annie getAnnie(){
+		return annie;
+	}
+
+	public Mordekaiser getMordekaiser(){
+		return mordekaiser;
+	}
+
+	public Azir getAzir(){
+		return azir;
+	}
+
+	public Kennen getKennen(){
+		return kennen;
+	}
+
+	public Riven getRiven(){
+		return riven;
+	}
+
+	public Chogath getChogath(){
+		return chogath;
+	}
+
+	public Aatrox getAatrox(){
+		return aatrox;
+	}
+
+	public Poppy getPoppy(){
+		return poppy;
+	}
+
+	public Taliyah getTaliyah(){
+		return taliyah;
+	}
+
+	public Illaoi getIllaoi(){
+		return illaoi;
+	}
+
+	public Pyke getPyke(){
+		return pyke;
+	}
+
+	public Heimerdinger getHeimerdinger(){
+		return heimerdinger;
+	}
+
+	public Alistar getAlistar(){
+		return alistar;
+	}
+
+	public XinZhao getXinZhao(){
+		return xinZhao;
+	}
+
+	public Lucian getLucian(){
+		return lucian;
+	}
+
+	public Volibear getVolibear(){
+		return volibear;
+	}
+
+	public Sejuani getSejuani(){
+		return sejuani;
+	}
+
+	public Nidalee getNidalee(){
+		return nidalee;
+	}
+
+	public Garen getGaren(){
+		return garen;
+	}
+
+	public Leona getLeona(){
+		return leona;
+	}
+
+	public Zed getZed(){
+		return zed;
+	}
+
+	public Blitzcrank getBlitzcrank(){
+		return blitzcrank;
+	}
+
+	public Rammus getRammus(){
+		return rammus;
+	}
+
+	public Velkoz getVelkoz(){
+		return velkoz;
+	}
+
+	public Caitlyn getCaitlyn(){
+		return caitlyn;
+	}
+
+	public Trundle getTrundle(){
+		return trundle;
+	}
+
+	public Kindred getKindred(){
+		return kindred;
+	}
+
+	public Akshan getAkshan(){
+		return akshan;
+	}
+
+	public Quinn getQuinn(){
+		return quinn;
+	}
+
+	public Ekko getEkko(){
+		return ekko;
+	}
+
+	public Nami getNami(){
+		return nami;
+	}
+
+	public Swain getSwain(){
+		return swain;
+	}
+
+	public Aphelios getAphelios(){
+		return aphelios;
+	}
+
+	public Sylas getSylas(){
+		return sylas;
+	}
+
+	public Taric getTaric(){
+		return taric;
+	}
+
+	public Syndra getSyndra(){
+		return syndra;
+	}
+
+	public Rakan getRakan(){
+		return rakan;
+	}
+
+	public Skarner getSkarner(){
+		return skarner;
+	}
+
+	public Gwen getGwen(){
+		return gwen;
+	}
+
+	public Braum getBraum(){
+		return braum;
+	}
+
+	public Veigar getVeigar(){
+		return veigar;
+	}
+
+	public Xerath getXerath(){
+		return xerath;
+	}
+
+	public Corki getCorki(){
+		return corki;
+	}
+
+	public Nautilus getNautilus(){
+		return nautilus;
+	}
+
+	public Ahri getAhri(){
+		return ahri;
+	}
+
+	public Jayce getJayce(){
+		return jayce;
+	}
+
+	public Darius getDarius(){
+		return darius;
+	}
+
+	public Tryndamere getTryndamere(){
+		return tryndamere;
+	}
+
+	public Janna getJanna(){
+		return janna;
+	}
+
+	public Elise getElise(){
+		return elise;
+	}
+
+	public Vayne getVayne(){
+		return vayne;
+	}
+
+	public Brand getBrand(){
+		return brand;
+	}
+
+	public Zoe getZoe(){
+		return zoe;
+	}
+
+	public Graves getGraves(){
+		return graves;
+	}
+
+	public Soraka getSoraka(){
+		return soraka;
+	}
+
+	public Xayah getXayah(){
+		return xayah;
+	}
+
+	public Viego getViego(){
+		return viego;
+	}
+
+	public Karthus getKarthus(){
+		return karthus;
+	}
+
+	public Vladimir getVladimir(){
+		return vladimir;
+	}
+
+	public Zilean getZilean(){
+		return zilean;
+	}
+
+	public Katarina getKatarina(){
+		return katarina;
+	}
+
+	public Shyvana getShyvana(){
+		return shyvana;
+	}
+
+	public Warwick getWarwick(){
+		return warwick;
+	}
+
+	public Ziggs getZiggs(){
+		return ziggs;
+	}
+
+	public Kled getKled(){
+		return kled;
+	}
+
+	public Khazix getKhazix(){
+		return khazix;
+	}
+
+	public Olaf getOlaf(){
+		return olaf;
+	}
+
+	public TwistedFate getTwistedFate(){
+		return twistedFate;
+	}
+
+	public Nunu getNunu(){
+		return nunu;
+	}
+
+	public Qiyana getQiyana(){
+		return qiyana;
+	}
+
+	public Rengar getRengar(){
+		return rengar;
+	}
+
+	public Bard getBard(){
+		return bard;
+	}
+
+	public Irelia getIrelia(){
+		return irelia;
+	}
+
+	public Ivern getIvern(){
+		return ivern;
+	}
+
+	public MonkeyKing getMonkeyKing(){
+		return monkeyKing;
+	}
+
+	public Ashe getAshe(){
+		return ashe;
+	}
+
+	public Kalista getKalista(){
+		return kalista;
+	}
+
+	public Akali getAkali(){
+		return akali;
+	}
+
+	public Vi getVi(){
+		return vi;
+	}
+
+	public Amumu getAmumu(){
+		return amumu;
+	}
+
+	public Lulu getLulu(){
+		return lulu;
+	}
+
+	public Morgana getMorgana(){
+		return morgana;
+	}
+
+	public Nocturne getNocturne(){
+		return nocturne;
+	}
+
+	public Diana getDiana(){
+		return diana;
+	}
+
+	public AurelionSol getAurelionSol(){
+		return aurelionSol;
+	}
+
+	public Zyra getZyra(){
+		return zyra;
+	}
+
+	public Viktor getViktor(){
+		return viktor;
+	}
+
+	public Cassiopeia getCassiopeia(){
+		return cassiopeia;
+	}
+
+	public Nasus getNasus(){
+		return nasus;
+	}
+
+	public Twitch getTwitch(){
+		return twitch;
+	}
+
+	public DrMundo getDrMundo(){
+		return drMundo;
+	}
+
+	public Orianna getOrianna(){
+		return orianna;
+	}
+
+	public Evelynn getEvelynn(){
+		return evelynn;
+	}
+
+	public RekSai getRekSai(){
+		return rekSai;
+	}
+
+	public Lux getLux(){
+		return lux;
+	}
+
+	public Sion getSion(){
+		return sion;
+	}
+
+	public Camille getCamille(){
+		return camille;
+	}
+
+	public MasterYi getMasterYi(){
+		return masterYi;
+	}
+
+	public Ryze getRyze(){
+		return ryze;
+	}
+
+	public Malphite getMalphite(){
+		return malphite;
+	}
+
+	public Anivia getAnivia(){
+		return anivia;
+	}
+
+	public Shen getShen(){
+		return shen;
+	}
+
+	public JarvanIV getJarvanIV(){
+		return jarvanIV;
+	}
+
+	public Malzahar getMalzahar(){
+		return malzahar;
+	}
+
+	public Zac getZac(){
+		return zac;
+	}
+
+	public Gragas getGragas(){
+		return gragas;
+	}
+}
