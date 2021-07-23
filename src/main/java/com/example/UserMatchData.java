890257a5159a80b001fcd36004e632341c85b6b9
Wayne Li <wayne121542@gmail.com>
2021-07-23 11:57:31 -0700

    added some methods which can be called to get some basic match data, will add more fields soon

--- src/main/java/com/example/UserMatchData.java
+++ src/main/java/com/example/UserMatchData.java
@@ -1,23 +1,43 @@
 package com.example;
 
+
+import com.google.gson.Gson;
 import model.JsonMember;
 import model.MatchResponse;
 
 
 public class UserMatchData {
-    int id;
+    final static Gson gson = new Gson();
+    //int id;
     int totalTime;
     int numFrames;
     MatchResponse response;
-    UserMatchData(MatchResponse matchResponse, int id){
-        numFrames = matchResponse.getFrames().size();
-        response = matchResponse;
-        this.id = id;
+    UserMatchData(int id, String matchId){// constructor for matchdata object, takes in a interger(1-10) as participant ID, and a match id
+        String matchString = Api.getMatchDataByMatchId(matchId);
+        response = gson.fromJson(matchString, MatchResponse.class);
+        numFrames = response.getFrames().size();
+
+        //this.id = id;
     }
-    public float getCs(){
+    //NOTE: all the following 5 methods take in the participant id(1-10) and return the corresponding value
+    public int getCs(int id){//returns cs score for a participant, id 1-10
         int maxCs = getJsonMemeberFromParticipantId(id,numFrames-1).getMinionsKilled();
         return maxCs;
     }
+    public int getTeamScore(int id){
+        return getJsonMemeberFromParticipantId(id, numFrames-1).getTeamScore();
+    }
+    public int getDominionScore(int id){
+        return getJsonMemeberFromParticipantId(id, numFrames-1).getDominionScore();
+    }
+    public int getTotalGold(int id){
+        return getJsonMemeberFromParticipantId(id, numFrames-1).getTotalGold();
+    }
+    public int getLevel(int id){
+        return getJsonMemeberFromParticipantId(id, numFrames-1).getLevel();
+    }
+
+
     public JsonMember getJsonMemeberFromParticipantId(int i, int frameNumber){
         return response.getFrames().get(frameNumber).getParticipantFrames().getJsonMemberI(i);
     }
