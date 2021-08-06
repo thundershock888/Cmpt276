7279288075fde46a69b194360a1f3abf25e7752b
Wayne Li <wayne121542@gmail.com>
2021-08-06 12:28:52 -0700

    fixed HTML alignment

---
+++ src/main/java/champs/Passive.java
@@ -0,0 +1,21 @@
+package champs;
+
+import java.awt.*;
+
+public class Passive{
+	private Image image;
+	private String name;
+	private String description;
+
+	public Image getImage(){
+		return image;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getDescription(){
+		return description;
+	}
+}
